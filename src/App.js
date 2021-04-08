159b1596cd76fcbeb4b9781d1459bf43dbe32c18
KatAlves <KatAlves@users.noreply.github.com>
2021-04-08 23:05:54 +0100

    Build

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ export default function App() {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
-    <Footer />;
+    
   }
 }
 
